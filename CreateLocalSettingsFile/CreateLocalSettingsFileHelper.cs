21a98740cfd6113ebe369ef5f48339cee207dfe7
Francesco Dell Ascenza <f.dell.ascenza@avanade.com>
2024-01-10 14:47:08 +0100

    Add project files.

---
+++ CreateLocalSettingsFile/CreateLocalSettingsFileHelper.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
+
+namespace CreateLocalSettingsFile
+{
+    public static class CreateLocalSettingsFileHelper
+    {
+
+        public static Dictionary<string, object> ReadCsvData(string filePath, string webJobsFilePath, string[] filters)
+        {
+            var data = new Dictionary<string, object>();
+            using var csvParser = new TextFieldParser(filePath);
+            csvParser.SetDelimiters(";");
+            using var csvWebJobsParser = new TextFieldParser(webJobsFilePath);
+            csvWebJobsParser.SetDelimiters(";");
+
+            while (!csvParser.EndOfData)
+            {
+                var fields = csvParser.ReadFields()!;
+                foreach (var filter in filters)
+                {
+                    if (!fields[3].Equals("N.A.") && fields[3] is not null && !fields[4].Equals("N.A."))
+                    {
+                        if ((fields[2] is not null && fields[2].Equals(filter)) && !data.Any(x => x.Key.Equals(fields[3])))
+                            data.Add(fields[3], fields[4]);
+                    }
+                }
+            }
+            while (!csvWebJobsParser.EndOfData)
+            {
+                var fields = csvWebJobsParser.ReadFields()!;
+                if (!data.Any(x => x.Key.Equals(fields[0])))
+                    data.Add(fields[0], fields[1]);
+            }
+
+            return data;
+        }
+
+        public static List<Dictionary<string, object>> CleanJsonDictionary(Dictionary<string, object> controlDictionary, params Dictionary<string, object>[] dictionariesToClean)
+        {
+            var returnList = new List<Dictionary<string, object>>();
+            var missingSettings = new Dictionary<string, object>();
+
+            for (int i = 0; i < dictionariesToClean.Length; i++)
+            {
+                var cleanedDictionaryTemp = new Dictionary<string, object>();
+
+                foreach (var couple in dictionariesToClean[i])
+                {
+                    if (!controlDictionary.ContainsKey(couple.Key))
+                        missingSettings.TryAdd(couple.Key, dictionariesToClean[0].Values);
+                    else if (controlDictionary[couple.Key] == null)
+                        missingSettings.TryAdd(couple.Key, couple.Value!);
+                    else
+                    {
+                        var valueType = controlDictionary[couple.Key].GetType();
+                        cleanedDictionaryTemp.TryAdd(
+                            couple.Key,
+                            couple.Value != null && valueType.IsInstanceOfType(couple.Value)
+                                ? couple.Value
+                                : Convert.ChangeType(controlDictionary[couple.Key], valueType)
+                        );
+                    }
+                }
+                returnList.Add(cleanedDictionaryTemp);
+            }
+            returnList.Insert(0, missingSettings);
+
+            return returnList;
+        }
+
+        public static void CreateJsonFile(string path, Dictionary<string, object> contentValues)
+        {
+            try
+            {
+                var resultObject = new
+                {
+                    IsEncrypted = false,
+                    Values = contentValues
+                };
+
+                File.WriteAllText(path, JsonConvert.SerializeObject(resultObject, Formatting.Indented));
+            }
+            catch (Exception) { throw; }
+        }
+
+        public static bool AreFilesTheSame(Dictionary<string, object> generatedDic, Dictionary<string, object> expectedDic)
+        {
+            bool flag = true;
+            foreach (var item in generatedDic)
+            {
+                if (!(expectedDic.TryGetValue(item.Key, out var expectedValue) && string.Equals(expectedValue?.ToString(), item.Value?.ToString())))
+                {
+                    Console.WriteLine($"Setting not found in original file: {item.Key}");
+                    flag = false;
+                }
+            }
+            foreach (var item in expectedDic)
+            {
+                if (!(generatedDic.TryGetValue(item.Key, out var expectedValue) && string.Equals(expectedValue?.ToString(), item.Value?.ToString())))
+                {
+                    Console.WriteLine($"Setting not found in generated file: {item.Key}");
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+    }
+}
---
+++ CreateLocalSettingsFile/Program.cs
@@ -0,0 +1,100 @@
+﻿using CreateLocalSettingsFile;
+using Newtonsoft.Json;
+
+string basePath = $"BASE-DIRECTORY";
+string jsonOriginalPath = $"ORIGINAL-CONFIG-FILE";
+string csvReportName = "REPORT-NAME.csv";
+string csvWebJobSettings = "WEB-JOBS-SETTINGS.csv";
+string outputFileNameHTTP = "OUTPUT-FILE-NAME1.json";
+string outputFileNameNotHTTP = "OUTPUT-FILE-NAME2.json";
+string outputFileNameCOMPARE = "OUTPUT-FILE-NAME3.json";
+
+
+Console.WriteLine($"Press a key to elaborate after dropping the report file in this path: {basePath}{csvReportName}");
+Console.ReadKey();
+
+try
+{
+    if (!File.Exists($"{basePath}{csvReportName}"))
+    {
+        Console.WriteLine("No file was elaborated, press a key to exit");
+        return;
+    }
+
+    Console.WriteLine("Elaborating...");
+
+    var httpFunctionsData = CreateLocalSettingsFileHelper.ReadCsvData($"{basePath}{csvReportName}", $"{basePath}{csvWebJobSettings}", ["HTTP", "ALL"]);
+    var otherFunctionsData = CreateLocalSettingsFileHelper.ReadCsvData($"{basePath}{csvReportName}", $"{basePath}{csvWebJobSettings}", ["ALL", "TimerTrigger", "BlobTrigger", "ServiceBus"]);
+
+    var allFunctionsData = httpFunctionsData
+        .Union(otherFunctionsData)
+        .ToDictionary(pair => pair.Key, pair => pair.Value
+    );
+
+    CreateLocalSettingsFileHelper.CreateJsonFile(
+        $"{basePath}reportSettingFileForHttpTriggered.json", 
+        httpFunctionsData.OrderBy(x => x.Key, StringComparer.OrdinalIgnoreCase).ToDictionary()
+    );
+    CreateLocalSettingsFileHelper.CreateJsonFile(
+        $"{basePath}reportSettingFileForNonHttpTriggered.json", 
+        otherFunctionsData.OrderBy(x => x.Key, StringComparer.OrdinalIgnoreCase).ToDictionary()
+    );
+    CreateLocalSettingsFileHelper.CreateJsonFile(
+        $"{basePath}reportSettingFileForAllFunctions.json", 
+        allFunctionsData.OrderBy(x => x.Key, StringComparer.OrdinalIgnoreCase).ToDictionary()
+    );
+
+    Console.WriteLine("Temp files created! Press a key to continue...");
+    Console.ReadKey();
+
+    dynamic jsonObject = JsonConvert.DeserializeObject(File.ReadAllText(jsonOriginalPath))!;
+    var originalSettings = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonObject.Values.ToString());
+
+    List<Dictionary<string, object>> cleanedDictionaries = CreateLocalSettingsFileHelper.CleanJsonDictionary(originalSettings, new Dictionary<string, object>[] { httpFunctionsData, otherFunctionsData, allFunctionsData });
+
+    if (cleanedDictionaries[0].Count() > 0)
+    {
+        Console.WriteLine("The following configs has been found without any corrispondency:");
+        Console.WriteLine(string.Join(Environment.NewLine, cleanedDictionaries[0].Select(kvp => kvp.Key)));
+    }
+    Console.WriteLine("-----------------------------------------------------------------------------------------");
+    Console.WriteLine($"The files {(CreateLocalSettingsFileHelper.AreFilesTheSame(cleanedDictionaries[3], originalSettings) ? "are not " : string.Empty) }the same; press a key to continue...");
+    Console.ReadKey();
+    Console.Clear();
+
+    // Creating file for HTTP
+    CreateLocalSettingsFileHelper.CreateJsonFile(
+       $"{basePath}\\OUTPUT\\{outputFileNameHTTP}",
+       cleanedDictionaries[1].OrderBy(x => x.Key, StringComparer.OrdinalIgnoreCase).ToDictionary()
+    );
+
+    // Creating file for OTHER TRIGGERS
+    CreateLocalSettingsFileHelper.CreateJsonFile(
+       $"{basePath}\\OUTPUT\\{outputFileNameNotHTTP}",
+       cleanedDictionaries[2].OrderBy(x => x.Key, StringComparer.OrdinalIgnoreCase).ToDictionary()
+    );
+
+    // Creating file for COMPARE
+    CreateLocalSettingsFileHelper.CreateJsonFile(
+       $"{basePath}\\OUTPUT\\{outputFileNameCOMPARE}",
+       cleanedDictionaries[3].OrderBy(x => x.Key, StringComparer.OrdinalIgnoreCase).ToDictionary()
+    );
+
+    Console.WriteLine("Ended! Press a key to exit...");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"The following exception was thrown:\n{e.Message}\n{e.StackTrace}\n\nPress a key to terminate.");
+}
+finally
+{
+    Console.ReadKey();
+}
+
+
+
+
+
+
+
+
